4f928aa646f36f161162a2885b942a6046c7c9ee
Nick Ashworth <jna2188@utulsa.edu>
2024-02-17 21:51:55 -0800

    added IMU control

--- cv.py
+++ cv.py
@@ -4,12 +4,88 @@ import math # needed for floor
 from scipy.spatial import distance # need to calc distance from center
 
 
+import os # needed for IMU
+import sys # needed for IMU
+import time # needed for IMU
+import smbus # needed for IMU
 
-#cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('/home/zee/m2-res_480p.mp4')
-#cap = cv2.VideoCapture('/home/zee/9zYxpT0f6jW2IVB5.mp4')
+from imusensor.MPU9250 import MPU9250 # needed for IMU
+from imusensor.filters import kalman # needed for IMU
+
+# set up IMU
+address = 0x68
+bus = smbus.SMBus(1)
+imu = MPU9250.MPU9250(bus, address)
+imu.begin()
+
+sensorfusion = kalman.Kalman()
+
+imu.readSensor()
+imu.computeOrientation()
+sensorfusion.roll = imu.roll
+sensorfusion.pitch = imu.pitch
+sensorfusion.yaw = imu.yaw
+
+count = 0
+currTime = time.time()
+
+# do one run to get defaults
+imu.readSensor()
+imu.computeOrientation()
+newTime = time.time()
+dt = newTime - currTime
+currTime = newTime
+sensorfusion.computeAndUpdateRollPitchYaw(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2], imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2],\
+												imu.MagVals[0], imu.MagVals[1], imu.MagVals[2], dt)
+defaultRoll = sensorfusion.roll
+defaultPitch = sensorfusion.pitch
+defaultYaw = sensorfusion.yaw
+
+
+cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture('test.mp4')
+#cap = cv2.VideoCapture('m2-res_480p.mp4')
+#cap = cv2.VideoCapture('9zYxpT0f6jW2IVB5.mp4')
 
 while True:
+    
+    imu.readSensor()
+    imu.computeOrientation()
+    newTime = time.time()
+    dt = newTime - currTime
+    currTime = newTime
+
+    sensorfusion.computeAndUpdateRollPitchYaw(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2], imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2],\
+												imu.MagVals[0], imu.MagVals[1], imu.MagVals[2], dt)
+                                                
+    roll = defaultRoll - sensorfusion.roll
+    pitch = defaultPitch - sensorfusion.pitch
+    yaw = defaultYaw - sensorfusion.yaw
+    
+    if roll > 100:
+        roll == 100
+    elif roll < -100:
+        roll == -100
+    else:
+        roll = round(roll)
+        
+    if pitch > 100:
+        pitch == 100
+    elif pitch < -100:
+        pitch == -100
+    else:
+        pitch = round(pitch) * 10
+        
+    if yaw > 100:
+        yaw == 100
+    elif yaw < -100:
+        yaw == -100
+    else:
+        yaw = round(yaw) * 10
+
+    #print(f"ROLL: {str(roll)} , PITCH: {str(pitch)} , YAW: {str(yaw)}")
+
+    
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -19,10 +95,17 @@ while True:
     # show the canny filter
     cv2.imshow('Video feed', mask)
     
-    #find center of image and draw it (blue circle)
+    #find center of image and draw it (green circle)
     (h, w) = img.shape[:2]
+    #print(f"{str(h)} {str(w)}")
     imageCenter = [w//2, h//2]
-    cv2.circle(img, (w//2, h//2), 7, (255, 255, 255), -1)
+    
+    modifiedW = w//2 + yaw
+    modifiedH = h//2 - pitch
+    
+    directionCenter = [modifiedW, modifiedH]
+    
+    cv2.circle(img, (modifiedW, modifiedH), 7, (0, 255, 0), -1)
     
     # find the contours in the edged image
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -50,8 +133,8 @@ while True:
             center_Y = int(M["m01"] / M["m00"])
             contour_center = (center_X, center_Y)
         
-            # calculate distance to image_center
-            distances_to_center = (distance.euclidean(imageCenter, contour_center))
+            # calculate distance to direction center
+            distances_to_center = (distance.euclidean(directionCenter, contour_center))
         
             # save to a list of dictionaries
             contourDistance.append({'contour': contour, 'center': contour_center, 'distance_to_center': distances_to_center})
@@ -59,15 +142,15 @@ while True:
         # sort the distances
         sortedDistances = sorted(contourDistance, key=lambda i: i['distance_to_center'])
     
-        # find contour of closest building to center and draw it (blue)
+        # find contour of closest building to center and draw it (green)
         try:
             centerContour = sortedDistances[0]['contour']
-            cv2.drawContours(img, [centerContour], 0, (255, 0, 0), 2)
+            cv2.drawContours(img, [centerContour], 0, (0, 255, 0), 2)
         except IndexError:
             print("contour error")
         
     else:
-        cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
+        cv2.drawContours(img, contours, -1, (0, 0, 255), 2)
     
     cv2.imshow('Video feed', img)
     
